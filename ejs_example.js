c764717d91f65a4d16b26f69e23fd03224dc5688
derekja <derekja@gmail.com>
2017-03-14 22:53:08 -0700

    adding readme and ejs examples

--- ejs_example.js
+++ ejs_example.js
@@ -1,10 +1,29 @@
 var express = require('express')
 var app = express()
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
 app.get('/', function(req, res) {
-    res.send('Hello World!')
+    res.render('simple1')
 })
 
-app.listen(8001, function() {
-    console.log('Example app listening on port 8001!')
+// about page 
+app.get('/about', function(req, res) {
+    var sentence = "this is a test about page, passed as a variable through ejs";
+    var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    res.render('about', {
+        drinks: drinks,
+        sentence: sentence
+    });
+});
+
+app.listen(8000, function() {
+    console.log('Example app listening on port 8000!')
 })
