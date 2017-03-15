ea9bd5de1533061797100d034bce189e0128e639
derekja <derekja@gmail.com>
2017-03-15 11:21:02 -0700

    add sql example

---
+++ sql_ejs_example.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+var connection = mysql.createConnection({
+    host: '206.12.96.242',
+    user: 'group0',
+    password: 'untanglingGroup0',
+    database: 'testDB'
+});
+connection.connect();
+
+var employees;
+
+connection.query('SELECT * FROM employees', function(err, rows, fields) {
+    if (err) throw err;
+
+    employees = rows;
+    console.log(rows[0]);
+});
+
+connection.end();
+
+app.get('/', function(req, res) {
+
+
+    res.render('simple1', { employees: employees })
+})
+
+// about page 
+app.get('/about', function(req, res) {
+    var sentence = "this is a test about page, passed as a variable through ejs";
+    var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    res.render('about', {
+        drinks: drinks,
+        sentence: sentence
+    });
+});
+
+app.listen(8000, function() {
+    console.log('Example app listening on port 8000!')
+})
